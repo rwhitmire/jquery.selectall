17388f09723195b6d4bd490c5c021a22af3a9d61
Ryan Whitmire <ryanw51@gmail.com>
2015-08-14 13:57:08 -0400

    Update jquery.selectall.js

--- jquery.selectall.js
+++ jquery.selectall.js
@@ -1,24 +1,30 @@
-$(document).on('click', '[data-js=selectAll]', e => {
-    $('[name=selected]').prop('checked', e.target.checked);
-    $('[name=selected]').trigger('change');
-});
+var $ = require('jquery');
 
-$(document).on('click', '[name=selected]', e => {
-    var count = $('[name=selected]').length;
+var setSelectAll = function () {
+    var total = $('[name=selected]').length;
     var checked = $('[name=selected]:checked').length;
 
     if (checked === 0) {
         $('[data-js=selectAll]').prop('indeterminate', false);
         $('[data-js=selectAll]').prop('checked', false);
-        return;
     }
-
-    if (checked === count) {
+    else if (checked === total) {
         $('[data-js=selectAll]').prop('indeterminate', false);
         $('[data-js=selectAll]').prop('checked', true);
-        return;
+    }
+    else {
+        $('[data-js=selectAll]').prop('checked', false);
+        $('[data-js=selectAll]').prop('indeterminate', true);
     }
 
-    $('[data-js=selectAll]').prop('checked', false);
-    $('[data-js=selectAll]').prop('indeterminate', true);
+    $(document).trigger('selectall:changed', { checked: checked, total: total });
+}
+
+$(document).on('click', '[data-js=selectAll]', e => {
+    $('[name=selected]').prop('checked', e.target.checked);
+    setSelectAll();
+});
+
+$(document).on('click', '[name=selected]', e => {
+    setSelectAll();
 });
